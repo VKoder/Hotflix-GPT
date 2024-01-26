fecf0b976de620222d3bd8380f22be6ca2fe24ef
VKoder <vivekkhule204@gmail.com>
2024-01-26 21:36:43 +0530

    Login and SignUp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import Header from "../components/Header";
 const Login = () => {
-  return (
+  
+
+    const[isSignIn, setisSignIn] = useState(true);
+
+    const  toggleSignInForm =()=> {
+        setisSignIn(!isSignIn)
+    }
+
+    return (
     <div className="relative w-12/12">
       <Header />
-      <div className="bg-black bg-opacity-75  lg:w-4/12 w-11/12 sm:w-7/12 md:w-5/12 absolute mx-auto lg:my-28 my-44 md:my-24 right-0 left-0 z-20 py-4 md:py-6 lg:py-6">
+      <div className="bg-black bg-opacity-75 rounded-md  lg:w-4/12 w-11/12 sm:w-7/12 md:w-5/12 absolute mx-auto lg:my-28 my-44 md:my-24 right-0 left-0 z-20 py-4 md:py-6 lg:py-6">
         <form className="flex justify-center items-center flex-col lg:py-8 lg:px-8 py-4 px-2 md:py-8 md:px-8">
           <div className="w-10/12">
-            <h1 className="text-white lg:text-4xl text-3xl md:text-4xl font-bold md:my-4 my-3 lg:my-4">Sign In</h1>
+        <h1 className="text-white lg:text-4xl text-3xl md:text-4xl font-bold md:my-4 my-3 lg:my-4">{isSignIn ? "Sign In" : "Sign Up"}</h1>
 
+            {!isSignIn && <input
+              className="lg:py-3 md:py-3 lg:text-base md:text-base text-sm py-3 bg-zinc-900 bg-opacity-60 text-white border-[1px] border-gray-400 rounded-md md:px-4 px-3 my-3 lg:px-4 w-full"
+              type="text"
+              placeholder="Full Name"
+            ></input>}
             <input
               className="lg:py-3 md:py-3 lg:text-base md:text-base text-sm py-3 bg-zinc-900 bg-opacity-60 text-white border-[1px] border-gray-400 rounded-md md:px-4 px-3 my-3 lg:px-4 w-full"
               type="text"
@@ -19,13 +33,13 @@ const Login = () => {
               placeholder="Password"
             ></input>
             <button className="w-full bg-red-600 py-2 text-white rounded-md my-3 font-semibold">
-              Sign In
+            {isSignIn ? "Sign In" : "Sign Up"}
             </button>
             <span className=" lg:text-base md:text-base text-sm font-normal text-gray-300">
-              New to Netflix?
+            {isSignIn ? "New to Netflix?" : "Already have an account?"}
             </span>
-            <span className="font-semibold lg:text-base md:text-base text-sm  text-gray-50 px-1.5 cursor-pointer">
-              Sign up now.
+            <span className="font-semibold lg:text-base md:text-base text-sm  text-gray-50 px-1.5 cursor-pointer" onClick={toggleSignInForm}>
+            {isSignIn ? "Sign Up Now" : "Sign In"}
             </span>
           </div>
         </form>
